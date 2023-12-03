b3714473b2c4ea5c8718aa59fa7be52402845f5e
Andrzej Pacek <andrze.pacek@gmail.com>
2023-12-03 16:29:55 +0100

    Zadanie dzień 4

--- ChallangeApp/ChallangeApp/Program.cs
+++ ChallangeApp/ChallangeApp/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var Name = "Andrzej";
+var sex = "Męzczyzna";
+var Age = 32;
+
+if (sex == "kobieta")
+{
+    if (Name == "Ewa")
+    {
+        Console.WriteLine("Ewa");
+    }
+    else
+    {
+    Console.WriteLine("Kobieta");
+    }
+
+}
+else
+{
+    Console.WriteLine("mężczyzna");
+}
+if (Age <18)
+{
+    Console.WriteLine("niepełnoletni/a");
+}
+else if (Age >= 18 && Age > 30) 
+{
+    Console.WriteLine("pełnoletni/a, po 30-stce");
+}
+else if (Age >= 30)
+{
+    if (Age == 33)
+    {
+        Console.WriteLine("lat 33");
+    }
+}
+else
+{
+    Console.WriteLine("po 30-stce");
+}
+
